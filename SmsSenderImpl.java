7865bde5ec06315232944dc6db8d711ec090870b
romainlavabre <romainlavabre98@gmail.com>
2023-06-29 14:25:13 +0200

    FIX bean name

--- SmsSenderImpl.java
+++ SmsSenderImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
-@Service
+@Service( "smsSender" )
 public class SmsSenderImpl implements SmsSender {
 
     protected final Environment environment;
